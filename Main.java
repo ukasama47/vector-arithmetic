d4e490c3a276d270dade5e660706f5a749f2eb94
uk454 <92313867+ukasama47@users.noreply.github.com>
2024-04-24 17:24:25 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+//1211201118 林　優花
+class Main {
+	//情報工学演習II 第13回課題サンプルプログラム
+	public static void main(String[] args) {
+		double[][] A1 = { { 1.0, 2.0, 3.0 }, { 4.0, 5.0, 6.0 } };
+		double[][] A2 = { { 7.0, 8.0, 9.0 }, { 10.0, 11.0, 12.0 } };
+
+		Mat mat = new Mat();
+		System.out.println("行列A1");
+		mat.show(A1);
+		System.out.println("行列A2");
+		mat.show(A2);
+
+		System.out.println("A1のスカラー倍");
+		mat.show(mat.sc(A1, 10));
+		System.out.println("A2のスカラー倍");
+		mat.show(mat.sc(A2, 10));
+
+		System.out.println("A1の転置行列");
+		mat.show(mat.T(A1));
+		System.out.println("A2の転置行列");
+		mat.show(mat.T(A2));
+
+		System.out.println("A1とA2の和");
+		mat.show(mat.add(A1, A2));
+		System.out.println("A1とA2の差");
+		mat.show(mat.sub(A1, A2));
+		System.out.println("A1とA2の要素ごとの積");
+		mat.show(mat.mul(A1, A2));
+		System.out.println("A1とA2の要素ごとの除算");
+		mat.show(mat.div(A1, A2));
+
+		System.out.println("「A1」と「A2の転置行列」の行列積");
+		mat.show(mat.matmul(A1, mat.T(A2)));
+	}
+}
---
+++ Mat.java
@@ -0,0 +1,91 @@
+//1211201118 林　優花
+//行列演算クラス
+public class Mat {
+	// コンストラクタ
+	public Mat() {
+
+	}
+
+	// 行列 A を表示する．
+	public void show(double[][] A) {
+		System.out.println("---");
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; j++) {
+				System.out.print(A[i][j] + ", ");
+			}
+			System.out.println();
+		}
+		System.out.println("---");
+	}
+	
+  public double[][] add(double[][] A1, double [][] A2) {
+    double[][] out = new double [A1.length][A1[0].length];
+		for (int i = 0; i < A1.length; i++) {
+			for (int j = 0; j < A1[0].length; j++) {
+			  out[i][j] = A1[i][j] + A2[i][j];
+			}
+		} 
+		return out;
+	}
+
+  public double[][] sub(double[][] A1, double [][] A2) {
+		double[][] out = new double [A1.length][A1[0].length];
+		for (int i = 0; i < A1.length; i++) {
+			for (int j = 0; j < A1[0].length; j++) {
+			  out[i][j] = A1[i][j] - A2[i][j];
+			}
+		} 
+		return out;
+	}
+	
+	public double[][] mul(double[][] A1, double [][] A2) {
+		double[][] out = new double [A1.length][A1[0].length];
+		for (int i = 0; i < A1.length; i++) {
+			for (int j = 0; j < A1[0].length; j++) {
+			  out[i][j] = A1[i][j] * A2[i][j];
+			}
+		} 
+		return out;
+	}
+
+	public double[][] div(double[][] A1, double [][] A2) {
+		double[][] out = new double [A1.length][A1[0].length];
+		for (int i = 0; i < A1.length; i++) {
+			for (int j = 0; j < A1[0].length; j++) {
+			  out[i][j] = A1[i][j] / A2[i][j];
+			}
+		} 
+		return out;
+	}
+
+	public double[][] sc(double[][] A1, double s) {
+		double[][] out = new double [A1.length][A1[0].length];
+		for (int i = 0; i < A1.length; i++) {
+			for (int j = 0; j < A1[0].length; j++) {
+			  out[i][j] = s * A1[i][j];
+			}
+		} 
+		return out;
+	}
+	public double[][] T(double[][] A1) {
+		double[][] out = new double [A1[0].length][A1.length];
+		for (int i = 0; i < A1.length; i++) {
+			for (int j = 0; j < A1[0].length; j++) {
+			  out[j][i] = A1[i][j];
+			}
+		}
+		return out;
+	}
+	
+	public double[][] matmul(double[][] A1, double [][] A2) {
+		double[][] out = new double [A1.length][A2[0].length];
+		  for (int i = 0; i < A1.length; i++) {
+			  for (int j = 0; j < A2[0].length; j++) {
+				  for (int k = 0; k < A1[0].length; k++) {
+					  out[i][j] += A1[i][k] * A2[k][j];
+				  }
+			  }
+		  } 
+		return out;
+	}
+}
